a8e7d0ba098e1d0bbcc39d16cb44c52f285e0634
Bartosz Art <bartosz.art@gmail.com>
2024-10-23 18:10:22 +0200

    Customize Radix UI theme

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -4,11 +4,7 @@ import React from 'react';
 const NewIssuePage = () => {
 	return (
 		<div className='max-w-xl space-y-3'>
-			<TextField.Root placeholder='Title'>
-				<TextField.Slot>
-					{/* <MagnifyingGlassIcon height="16" width="16" /> */}
-				</TextField.Slot>
-			</TextField.Root>
+			<TextField.Root placeholder='Title' />
 			<TextArea placeholder='Description' />
 			<Button>Submit New Issue</Button>
 		</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import '@radix-ui/themes/styles.css';
-import './globals.css';
+import './globals.scss';
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import NavBar from '@/components/layout/NavBar';
 import { Theme } from '@radix-ui/themes';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -16,8 +19,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body>
-				<Theme>
+			<body className={inter.variable}>
+				<Theme accentColor='violet'>
 					<NavBar />
 					<main className='p-5'>{children}</main>
 				</Theme>
