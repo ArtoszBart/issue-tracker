30c0bd035051127e4bb8a3ae84cb916ee9d811b2
Bartosz Art <bartosz.art@gmail.com>
2024-10-24 13:39:09 +0200

    Show the issues

--- src/repository/issueRepository.ts
+++ src/repository/issueRepository.ts
@@ -1,6 +1,10 @@
 import prisma from '@/config/prismaClient';
 import IIssue, { NewIssue } from '@/models/issue';
 
+export async function getIssues(): Promise<IIssue[]> {
+	return await prisma.issue.findMany();
+}
+
 export async function createIssue(issue: NewIssue): Promise<IIssue> {
 	return await prisma.issue.create({
 		data: {
