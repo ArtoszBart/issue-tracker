7446bd98a410c7dd0a2aff81e9b57367096f643d
Bartosz Art <bartosz.art@gmail.com>
2024-10-25 11:21:09 +0200

    Show the deletion confirmation dialog box

--- src/components/DeleteIssueButton.tsx
+++ src/components/DeleteIssueButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
 
 interface IProps {
@@ -7,10 +7,32 @@ interface IProps {
 
 const DeleteIssueButton = ({ issueId }: IProps) => {
 	return (
-		<Button color='red'>
-			<TrashIcon />
-			Delete Issue
-		</Button>
+		<AlertDialog.Root>
+			<AlertDialog.Trigger>
+				<Button color='red'>
+					<TrashIcon />
+					Delete Issue
+				</Button>
+			</AlertDialog.Trigger>
+			<AlertDialog.Content maxWidth='450px'>
+				<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+				<AlertDialog.Description size='2'>
+					Are you sure you want to delete this issue? This action
+					cannot be undone.
+				</AlertDialog.Description>
+
+				<Flex gap='3' mt='4' justify='end'>
+					<AlertDialog.Cancel>
+						<Button variant='soft' color='gray'>
+							Cancel
+						</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button color='red'>Delete Issue</Button>
+					</AlertDialog.Action>
+				</Flex>
+			</AlertDialog.Content>
+		</AlertDialog.Root>
 	);
 };
 
