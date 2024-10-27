2a4e445a2d5ecfa3ec3a52f28e7dba16df865065
Bartosz Art <bartosz.art@gmail.com>
2024-10-28 00:32:33 +0100

    Implement pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-	return <div></div>;
+import Pagination from '@/components/Pagination';
+
+interface IProps {
+	searchParams: { page: string };
+}
+
+export default function Home({ searchParams: { page } }: IProps) {
+	return (
+		<Pagination itemCount={100} pageSize={10} currentPage={Number(page)} />
+	);
 }
